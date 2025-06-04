f31ab2142972779e6c62e112c859511a8c9c5633
kutteai <kutteai978@gmail.com>
2025-06-04 02:40:36 +0100

    f

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
 
-		 $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
+		 $middleware->api(append:[\Illuminate\Http\Middleware\HandleCors::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
